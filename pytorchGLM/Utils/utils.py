05029816cc9675e69eb782136640ee9111a731d5
parker lab <139789779+theparkerlab@users.noreply.github.com>
2024-02-17 01:24:54 -0500

    Added code to calculate egocentric array for each frame

--- pytorchGLM/Utils/utils.py
+++ pytorchGLM/Utils/utils.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import math
 
 ########## Checks if path exists, if not then creates directory ##########
 def check_path(basepath, path):
@@ -164,3 +164,153 @@ def h5load(filename):
     data = store['mydata']
     metadata = store.get_storer('mydata').attrs.metadata
     return data, metadata
+
+
+def rotate(origin, point, angle):
+    """
+    Rotate a point counterclockwise by a given angle around a given origin.
+
+    The angle should be given in radians.
+    """
+    ox, oy = origin
+    px, py = point
+
+    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+    return qx, qy
+def lineLineIntersection(A, B, C, D):
+    # Line AB represented as a1x + b1y = c1
+    a1 = B[1] - A[1]
+    b1 = A[0] - B[0]
+    c1 = a1*(A[0]) + b1*(A[1])
+ 
+    # Line CD represented as a2x + b2y = c2
+    a2 = D[1] - C[1]
+    b2 = C[0] - D[0]
+    c2 = a2*(C[0]) + b2*(C[1])
+ 
+    determinant = a1*b2 - a2*b1
+ 
+    if (determinant == 0):
+        # The lines are parallel. This is simplified
+        # by returning a pair of FLT_MAX
+        return 'parallel'
+    else:
+        x = (b2*c1 - b1*c2)/determinant
+        y = (a1*c2 - a2*c1)/determinant
+        return (x, y)
+
+def ebc_bins(bin_size_angle=3,bin_size_distance=40): 
+    top_left_corner = (60.152496337890625, 32.62717056274414)
+    top_right_corner = (664.1357421875, 27.67728614807129)
+    bottom_left_corner = (43.63418197631836, 493.8233337402344)
+    bottom_right_corner = (690.4715576171875, 490.5800476074219)
+    angle_bins = np.arange(0,360+bin_size_angle,bin_size_angle)
+    diagonal_distance_arena = math.hypot(top_right_corner[0] - bottom_left_corner[0], top_right_corner[1] - bottom_left_corner[1])
+    distance_bins = np.arange(0,diagonal_distance_arena+bin_size_distance,bin_size_distance)
+    return distance_bins,angle_bins
+
+def calaculate_ebc(center_neck_x,center_neck_y,center_haunch_x,center_haunch_y):
+    top_left_corner = (60.152496337890625, 32.62717056274414)
+    top_right_corner = (664.1357421875, 27.67728614807129)
+    bottom_left_corner = (43.63418197631836, 493.8233337402344)
+    bottom_right_corner = (690.4715576171875, 490.5800476074219)
+    distance_bins,angle_bins = ebc_bins()
+    ebc_data_final = []
+    for i in range(len(center_neck_x)):
+        ebc_bins_total = np.zeros((len(distance_bins)-1,len(angle_bins)-1))
+        for angle in range(0,360,3):
+            #center_neck_pos = (frame['center_neck_x'],frame['center_neck_y'])
+            #center_haunch_pos = (frame['center_haunch_x'],frame['center_haunch_y'])
+            center_neck_pos = (center_neck_x[i],center_neck_y[i])
+            center_haunch_pos = (center_haunch_x[i],center_haunch_y[[i]]) 
+            center_neck_pos = rotate(center_haunch_pos,center_neck_pos,angle=math.radians(-1*angle))
+            body_angle_radian_frame = math.atan2(center_haunch_pos[1]-center_neck_pos[1],center_haunch_pos[0]-center_neck_pos[0])
+            body_angle_deg_frame = math.degrees(body_angle_radian_frame)
+
+            if body_angle_deg_frame<0:
+                body_angle_deg_frame = 360+body_angle_deg_frame
+
+            
+
+            if(body_angle_deg_frame==0):
+                #left wall
+                interpoint = lineLineIntersection(center_haunch_pos,center_neck_pos,bottom_left_corner,top_left_corner)
+                min_distance = math.hypot(interpoint[0]-center_neck_pos[0],interpoint[1]-center_neck_pos[1])
+                distance_bin_index = np.digitize(min_distance,distance_bins)
+                angle_bin_index = np.digitize(angle,angle_bins)
+                ebc_bins_total[distance_bin_index-1][angle_bin_index]+=1
+
+            elif(body_angle_deg_frame>0 and body_angle_deg_frame<90):
+                #left wall and top wall
+                interpoint_l = lineLineIntersection(center_haunch_pos,center_neck_pos,bottom_left_corner,top_left_corner)
+                interpoint_t = lineLineIntersection(center_haunch_pos,center_neck_pos,top_left_corner,top_right_corner)
+                distance_from_point_l = math.hypot(interpoint_l[0]-center_neck_pos[0],interpoint_l[1]-center_neck_pos[1])
+                distance_from_point_t = math.hypot(interpoint_t[0]-center_neck_pos[0],interpoint_t[1]-center_neck_pos[1])
+                min_distance = min(distance_from_point_l,distance_from_point_t)
+                distance_bin_index = np.digitize(min_distance,distance_bins)
+                angle_bin_index = np.digitize(angle,angle_bins)
+                ebc_bins_total[distance_bin_index-1][angle_bin_index-1]+=1
+            
+            elif(body_angle_deg_frame==90):
+                #top wall
+                interpoint = lineLineIntersection(center_haunch_pos,center_neck_pos,top_left_corner,top_right_corner)
+                min_distance = math.hypot(interpoint[0]-center_neck_pos[0],interpoint[1]-center_neck_pos[1])
+                distance_bin_index = np.digitize(min_distance,distance_bins)
+                angle_bin_index = np.digitize(angle,angle_bins)
+                ebc_bins_total[distance_bin_index-1][angle_bin_index-1]+=1
+
+            elif(body_angle_deg_frame>90 and body_angle_deg_frame<180):
+                #top wall and right wall
+                interpoint_l = lineLineIntersection(center_haunch_pos,center_neck_pos,bottom_right_corner,top_right_corner)
+                interpoint_t = lineLineIntersection(center_haunch_pos,center_neck_pos,top_left_corner,top_right_corner)
+                distance_from_point_l = math.hypot(interpoint_l[0]-center_neck_pos[0],interpoint_l[1]-center_neck_pos[1])
+                distance_from_point_t = math.hypot(interpoint_t[0]-center_neck_pos[0],interpoint_t[1]-center_neck_pos[1])
+                min_distance = min(distance_from_point_l,distance_from_point_t)
+                distance_bin_index = np.digitize(min_distance,distance_bins)
+                angle_bin_index = np.digitize(angle,angle_bins)
+                ebc_bins_total[distance_bin_index-1][angle_bin_index-1]+=1
+                
+            elif(body_angle_deg_frame==180):
+                #right wall
+                interpoint = lineLineIntersection(center_haunch_pos,center_neck_pos,bottom_right_corner,top_right_corner)
+                min_distance = math.hypot(interpoint[0]-center_neck_pos[0],interpoint[1]-center_neck_pos[1])
+                distance_bin_index = np.digitize(min_distance,distance_bins)
+                angle_bin_index = np.digitize(angle,angle_bins) 
+                ebc_bins_total[distance_bin_index-1][angle_bin_index-1]+=1
+
+            elif(body_angle_deg_frame>180 and body_angle_deg_frame<270):
+                #right wall and bottom wall
+                
+                interpoint_l = lineLineIntersection(center_haunch_pos,center_neck_pos,bottom_right_corner,top_right_corner)
+                interpoint_t = lineLineIntersection(center_haunch_pos,center_neck_pos,bottom_left_corner,bottom_right_corner)
+                distance_from_point_l = math.hypot(interpoint_l[0]-center_neck_pos[0],interpoint_l[1]-center_neck_pos[1])
+                distance_from_point_t = math.hypot(interpoint_t[0]-center_neck_pos[0],interpoint_t[1]-center_neck_pos[1])
+                min_distance = min(distance_from_point_l,distance_from_point_t)
+                distance_bin_index = np.digitize(min_distance,distance_bins)
+                angle_bin_index = np.digitize(angle,angle_bins)
+                ebc_bins_total[distance_bin_index-1][angle_bin_index-1]+=1
+            
+            elif(body_angle_deg_frame == 270):
+                #bottom wall
+                interpoint = lineLineIntersection(center_haunch_pos,center_neck_pos,bottom_right_corner,bottom_left_corner)
+                min_distance = math.hypot(interpoint[0]-center_neck_pos[0],interpoint[1]-center_neck_pos[1])
+                distance_bin_index = np.digitize(min_distance,distance_bins)
+                angle_bin_index = np.digitize(angle,angle_bins)
+                ebc_bins_total[distance_bin_index-1][angle_bin_index-1]+=1
+            
+            else:
+                #bottom wall and left wall
+                interpoint_l = lineLineIntersection(center_haunch_pos,center_neck_pos,bottom_left_corner,top_left_corner)
+                interpoint_t = lineLineIntersection(center_haunch_pos,center_neck_pos,bottom_left_corner,bottom_right_corner)
+                distance_from_point_l = math.hypot(interpoint_l[0]-center_neck_pos[0],interpoint_l[1]-center_neck_pos[1])
+                distance_from_point_t = math.hypot(interpoint_t[0]-center_neck_pos[0],interpoint_t[1]-center_neck_pos[1])
+                min_distance = min(distance_from_point_l,distance_from_point_t)
+                distance_bin_index = np.digitize(min_distance,distance_bins)
+                if min_distance<=800:
+                    angle_bin_index = np.digitize(angle,angle_bins)
+                    #print(i,min_distance,distance_bin_index,angle_bin_index)
+                    ebc_bins_total[distance_bin_index-1][angle_bin_index-1]+=1
+        
+        ebc_data_final.append(ebc_bins_total)
+    return np.array(ebc_data_final)
